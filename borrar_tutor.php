f3e70a28bae7da4d2abb429d21853c4c61a5acab
OscarSotoN <38505650+OscarSotoN@users.noreply.github.com>
2018-08-09 21:48:21 -0400

    Add files via upload

--- borrar_tutor.php
+++ borrar_tutor.php
@@ -1,5 +1,5 @@
 <?php
 	$pdo = new PDO("mysql:dbname=tutorias;host=localhost", "root", "");
-	$statement = $pdo->prepare('DELETE FROM user WHERE nombre=');
+	$statement = $pdo->prepare('DELETE FROM tutores WHERE Nombre= "rrrrrr" ');
 	$statement->execute();
 ?>
